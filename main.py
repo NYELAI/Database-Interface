38d9b4a171449b46f1730386dbdda89c0b4150b3
Maudood Fareed <maudoodfareed0212@gmail.com>
2024-10-09 02:36:42 +0500

    initial commit

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ st.set_page_config(page_title="Main Page", layout="centered")
 
 
 st.title("Astra DB CRUD Operations with Streamlit")
+
+# Adding radio buttons for database selection
+db_choice = st.radio(
+    "Select Database Environment:",
+    ('Production Database', 'Development')
+)
